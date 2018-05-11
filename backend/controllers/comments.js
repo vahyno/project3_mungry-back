9441c1f67dfc2b2479626a1c5c64decebdd0de29
vahyno <vahyno@gmail.com>
2018-05-11 14:16:32 -0700

    controller create,delete comment

--- backend/controllers/comments.js
+++ backend/controllers/comments.js
@@ -15,9 +15,45 @@ function index(req, res) {
   });
 }
 
+function create(req, res){
+  Comment.create(req.body, function(err, createdComment){
+    if (err){
+      console.log('createdComment err: ', err);
+      res.send(err);
+    } else {
+      Recipe.findById(req.params.recipe_id, function(err, foundRecipe){
+        if (err){
+          console.log('foundRecipe err: ',err);
+          res.send(err);
+        }else {
+          recipe.comments.push(createdComment);
+          recipe.save();
+          res.json(createdComment);
+          console.log(createdComment);
+        }
+      });
+    }
+  });
+}
 
-
-
+function destroy(req, res){
+  Comment.findByIdAndRemove(req.params.comment_id, function(err, deletedComment){
+    if (err){
+      console.log('deletedComment err:',err);
+      res.send(err);
+    } else {
+      Recipe.findByIdAndUpdate(req.params.recipe_id,
+      {$pull: {comments: {_id: req.params.comment_id}}}, function (err){
+        if (err){
+          console.log('deletedComment err :',err);
+          res.send(err);
+        } else {
+          res.send('Success: deletedComment')
+        }
+      });
+    }
+  });
+}
 
 module.exports.index= index;
 module.exports.create = create;
