4c362d3a0118bc3c64012d8c4b9277da82ef9155
vahyno <vahyno@gmail.com>
2018-05-11 16:15:05 -0700

    backend heroku deployment

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,12 +1,12 @@
 var mongoose = require("mongoose");
+var dbName = 'mongodb://localhost/project3_mungry';
 
 // Map Global Promise to Resolve Mongo Promise Warning
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost/front_end", {
-	useMongoClient: true
-})
-  .then(() => console.log('Mongodb connected...'))
-  .catch(err => console.log(err));
+
+mongoose.connect(process.env.MONGODB_URI || dbName )
+	.then(() => console.log('Mongodb connected...'))
+	.catch(err => console.log(err));
 
 module.exports.Recipe = require("./recipe");
 module.exports.Comment = require("./comment");
