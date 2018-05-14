ae18e310d947f1fe49c022764fe51c05deb357c6
vahyno <vahyno@gmail.com>
2018-05-14 10:50:40 -0700

    form is working

--- frontend/src/components/NewRecipe.js
+++ frontend/src/components/NewRecipe.js
@@ -4,17 +4,55 @@ import RecipesModel from '../models/RecipesModel';
 
 class NewRecipe extends Component {
   state = {
-    post: '',
+    title: '',
+    description: '',
+    ingredients: '',
+    directions: '',
+    thumbnail: '',
   }
 
 
 
-  onInputChange = (e) => {
+  handleTitleChange = (e) => {
+    this.setState({
+      title: e.target.value,
+    });
+    console.log(this.title);
+  }
+
+  handleDescriptionChange = (e) => {
+    this.setState({
+      description: e.target.value,
+    });
+    console.log(this.description);
 
   }
+
+  handleIngredientsChange = (e) => {
+    this.setState({
+      ingredients: e.target.value,
+    });
+    console.log(this.ingredients);
+  }
+
+  handleDirectionsChange = (e) => {
+    this.setState({
+      directions: e.target.value,
+    });
+    console.log(this.directions);
+  }
+
+  handleThumbnailChange = (e) => {
+    this.setState({
+      thumbnail: e.target.value,
+    });
+    console.log(this.thumbnail);
+  }
+
 
   onFormSubmit = (e)=> {
-
+    e.preventDefault();
+    console.log('Form Submit!')
   }
 
   render(){
@@ -27,24 +65,24 @@ class NewRecipe extends Component {
         <div className="column">
           <form className="col s12" onSubmit={this.onFormSubmit}>
             <div className="input-field col s12">
-                <input onChange={ this.onInputChange } value={this.state.post} placeholder="Title" id="title" type="text" className="validate"/>
+                <input onChange={ this.handleTitleChange } value={this.state.title} placeholder="Title" id="title" type="text" className="validate"/>
               </div>
               <div className="input-field col s12">
-                <input onChange={ this.onInputChange } value={this.state.post} placeholder="Description" id="Description" type="text" className="validate"/>
+                <input onChange={ this.handleDescriptionChange } value={this.state.description} placeholder="Description" id="Description" type="text" className="validate"/>
               </div>
               <div className="input-field col s12" >
-                <textarea cols="40" rows="5" onChange={ this.onInputChange } value={this.state.post} placeholder="Ingredients" className="validate" id="ingredients"></textarea>
+                <textarea cols="40" rows="5" onChange={ this.handleIngredientsChange } value={this.state.ingredients} placeholder="Ingredients" className="validate" id="ingredients"></textarea>
               </div>
               <div className="input-field col s12" >
-                <textarea cols="40" rows="5" onChange={ this.onInputChange } value={this.state.post} placeholder="Directions" className="validate" id="directions"></textarea>
+                <textarea cols="40" rows="5" onChange={ this.handleDirectionsChange } value={this.state.directions} placeholder="Directions" className="validate" id="directions"></textarea>
               </div>
               <div className="input-field col s12">
-                <input onChange={ this.onInputChange } value={this.state.post} placeholder="Add image source" id="image" type="text" className="validate"/>
+                <input onChange={ this.handleThumbnailChange } value={this.state.thumbnail} placeholder="Add image source" id="image" type="text" className="validate"/>
               </div>
-          </form>
              <button className="btn waves-effect waves-light" type="submit" name="action">Create
-            <i className="material-icons right"></i>
-          </button>
+               <i className="material-icons right"></i>
+             </button>
+          </form>
         </div>
 
     )
